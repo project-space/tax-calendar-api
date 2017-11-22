9f1d415934cf74a9bfea7837e59c85a7d35906f8
alex troshkin <alextroshkin@outlook.com>
2017-11-23 02:33:42 +0300

    Change TaxPeriodCreatorTests - ENVD

--- tests/Business.Tests/TaxPeriodCreatorTests.fs
+++ tests/Business.Tests/TaxPeriodCreatorTests.fs
@@ -3,15 +3,16 @@ module TaxPeriodCreatorTests
 open Expecto
 open Business
 open Design.Models
+open Design.Models
 
 [<Tests>]
 let ``tests for Create`` =
     testList "Создание периодов для ЕНВД" [
         let withEnvdValidityPeriod test () =
-            let validityPeriod = [
+            let validityPeriod =
                 { Tax = TaxType.ENVD
                   IntroductionYear = uint16(2003) 
-                  CancellationYear = uint16(2021) }]
+                  CancellationYear = uint16(2021) }
             
             let templatePeriod = 
                 { Id = uint64(0)
@@ -24,31 +25,46 @@ let ``tests for Create`` =
             test (validityPeriod, templatePeriod)
 
         yield! testFixture withEnvdValidityPeriod [
-            "Должны быть созданы периоды начиная с года введения налога",
-            fun (validityPeriods, templatePeriod) ->
+            "Должны быть созданы квартальные периоды за года введения налога",
+            fun (validityPeriod, templatePeriod) ->
                 let expected = [
-                    { templatePeriod with Quarter = uint8(1); Year = uint16(2003) }
-                    { templatePeriod with Quarter = uint8(2); Year = uint16(2003) }
-                    { templatePeriod with Quarter = uint8(3); Year = uint16(2003) }
-                    { templatePeriod with Quarter = uint8(4); Year = uint16(2003) }
+                    { templatePeriod with Quarter = uint8(1); Year = validityPeriod.IntroductionYear }
+                    { templatePeriod with Quarter = uint8(2); Year = validityPeriod.IntroductionYear }
+                    { templatePeriod with Quarter = uint8(3); Year = validityPeriod.IntroductionYear }
+                    { templatePeriod with Quarter = uint8(4); Year = validityPeriod.IntroductionYear }
                 ]
 
-                let actual = TaxPeriodCreator.Create validityPeriods
+                let actual = TaxPeriodCreator.Create [validityPeriod]
 
-                Expect.containsAll actual expected "Нет квартальных периодов на год введения налога"
+                Expect.containsAll actual expected "Нет квартальных периодов за год введения налога"
 
-            "Должны быть созданы периоды до года отмены налога",
-            fun (validityPeriods, templatePeriod) ->
+            "Должны быть созданы квартальные периоды за последний год действительности налога",
+            fun (validityPeriod, templatePeriod) ->
                 let expected = [
-                    { templatePeriod with Quarter = uint8(1); Year = uint16(2021) }
-                    { templatePeriod with Quarter = uint8(2); Year = uint16(2021) }
-                    { templatePeriod with Quarter = uint8(3); Year = uint16(2021) }
-                    { templatePeriod with Quarter = uint8(4); Year = uint16(2021) }
+                    { templatePeriod with Quarter = uint8(1); Year = validityPeriod.CancellationYear - uint16(1) }
+                    { templatePeriod with Quarter = uint8(2); Year = validityPeriod.CancellationYear - uint16(1) }
+                    { templatePeriod with Quarter = uint8(3); Year = validityPeriod.CancellationYear - uint16(1) }
+                    { templatePeriod with Quarter = uint8(4); Year = validityPeriod.CancellationYear - uint16(1) }
                 ]
 
-                let actual = TaxPeriodCreator.Create validityPeriods
+                let actual = TaxPeriodCreator.Create [validityPeriod]
 
-                Expect.containsAll actual expected "Нет квартальных периодов на год введения налога"
+                Expect.containsAll actual expected "Нет квартальных периодов за последний год действительности налога"
+
+            "Не должны быть созданы периоды до введения налога",
+            fun (validityPeriod, _) ->
+                let actual = TaxPeriodCreator.Create [validityPeriod]
+                let hasWrongPeriods = List.exists (fun period -> period.Year < validityPeriod.IntroductionYear) actual
+
+                Expect.isFalse hasWrongPeriods "Есть периоды до введения налога"
+
+
+            "Не должны быть созданы периоды после отмены налога",
+            fun (validityPeriod, _) ->
+                let actual = TaxPeriodCreator.Create [validityPeriod]
+                let hasWrongPeriods = List.exists (fun period -> period.Year >= validityPeriod.CancellationYear) actual
+
+                Expect.isFalse hasWrongPeriods "Есть периоды после отмены налога"
 
 
             "Должны быть созданы все 4 квартальных периода в году",
@@ -60,7 +76,7 @@ let ``tests for Create`` =
                     { templatePeriod with Quarter = uint8(4) }
                 ]
 
-                let actual = TaxPeriodCreator.Create validityPeriods
+                let actual = TaxPeriodCreator.Create [validityPeriods]
 
                 Expect.containsAll actual expected "Созданы не все квартальные периоды"
         ]
