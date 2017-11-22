36d3d74cba4e8257fa0ba04dd22bbd53eb035426
alex troshkin <alextroshkin@outlook.com>
2017-11-23 01:53:22 +0300

    Added Create ENVD periods to TaxPeriodCreator

--- src/Business/TaxPeriodCreator.fs
+++ src/Business/TaxPeriodCreator.fs
@@ -1,17 +1,29 @@
 namespace Business
 
+
 module TaxPeriodCreator =
     open Design.Models
 
-    let public Create (validityPeriods: TaxValidityPeriod list) =
-        [
+    let private ``Create ENVD periods`` (validityPeriod: TaxValidityPeriod) =
+        let templatePeriod = 
             { Id = uint64(0)
               Tax = TaxType.ENVD
               Type = TaxPeriodType.Quarterly
-              Year = uint16(2003)
-              Quarter = uint8(1)
+              Year = uint16(0)
+              Quarter = uint8(0)
               Month = uint8(0) }
-        ]
 
-    let private ``Create ENVD periods`` validityPeriod =
-        []    
+        List.collect (fun year -> 
+            List.map (fun quarter -> 
+                { templatePeriod with Year = year; Quarter = quarter }
+            ) [uint8(1) .. uint8(4)]
+        ) [validityPeriod.IntroductionYear .. validityPeriod.CancellationYear]
+
+    let public Create (validityPeriods: TaxValidityPeriod list) =
+        let toTaxPeriods validityPeriod =
+            match validityPeriod with
+                | period when period.Tax = TaxType.ENVD -> ``Create ENVD periods`` period 
+                | period ->
+                    failwith (sprintf "Неподдерживаемый налог [%s]" (period.Tax.ToString()))
+
+        List.collect toTaxPeriods validityPeriods
