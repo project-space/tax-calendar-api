2b5508bec74d22c0abb2ea713e4b5e40d517a135
alex troshkin <alextroshkin@outlook.com>
2017-11-26 00:45:23 +0300

    Added settings module to data access

---
+++ src/DataAccess/Queries.fs
@@ -0,0 +1,55 @@
+namespace DataAccess
+
+open Core.Query
+open Design.Models.Settings
+open Newtonsoft.Json
+
+module Queries =
+
+    module Settings =
+
+        let public Get (firmId: uint64) (year: uint16) = async {
+            let script = @"
+                select
+                    Values 
+                form 
+                    YearSetting 
+                where 
+                    FirmId = @FirmId and 
+                    Year = @Year"
+
+            let param =
+                dict [
+                    "FirmId" => firmId
+                    "Year" => year
+                ]
+
+            let! serializedValues = QuerySingleAsync script param
+            let desirealizedValue = JsonConvert.DeserializeObject<YearSettingsValues>(serializedValues)
+
+            return { FirmId = firmId; Year = year; Values = desirealizedValue }
+        }
+
+
+        let public Save (settings: YearSettings) = async {
+            let script = @"
+                if exists (select 1 from YearSetting where FirmId = @FirmId and Year = @Year)
+                begin
+                    update YearSetting set
+                        Values = @Values
+                end
+                else begin
+                    insert into YearSetting
+                        values (@FirmId, @Year, @Values)
+                end"
+
+            let serializedValues = JsonConvert.SerializeObject(settings.Values)
+            let param = 
+                dict [
+                    "FirmId" => settings.FirmId
+                    "Year" => settings.Year
+                    "Values" => serializedValues
+                ]
+
+            return! ExecuteAsync script param
+        }
--- src/Design/Models/Settings.fs
+++ src/Design/Models/Settings.fs
@@ -1,14 +1,18 @@
 namespace Design.Models
 
-type TaxationSystem =
-    | OSNO
-    | USN
-    | ENVD
-    | PSN
-    | ESHN
+module Settings =
+    type TaxationSystem =
+        | OSNO
+        | USN
+        | ENVD
+        | PSN
+        | ESHN
 
-(* Данные о пользователе (за конкретный год) используемые для генерации событий *)
-type YearSettings = 
-    { FirmId: uint64
-      Year: uint16 
-      TaxationSystem: TaxationSystem }
+    (* Данные о пользователе (за конкретный год) используемые для генерации событий *)
+    type YearSettingsValues = 
+        { TaxationSystem: TaxationSystem }
+
+    type YearSettings = 
+        { FirmId: uint64
+          Year: uint16 
+          Values: YearSettingsValues }
