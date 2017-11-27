eea994cd0539526193b313dd7d434ce6bee91ac9
alex troshkin <alextroshkin@outlook.com>
2017-11-28 00:37:06 +0300

    Change Calendar models

--- src/Design/Models/Calendar.fs
+++ src/Design/Models/Calendar.fs
@@ -5,21 +5,23 @@ module Calendar =
     open System        
     
     (* Сущности являющиеся причиной события *)
+    [<FlagsAttribute>]
     type EventEntityType =
-        | Tax
+        | Tax = 1
 
     (* Возможные состояния события *)
+    [<FlagsAttribute>]
     type EventState =
-        | Default   = 0
-        | Removed   = 1
-        | Completed = 2
+        | Default   = 1
+        | Removed   = 2
+        | Completed = 3
 
     (* Конкретное событие *)
     type Event =
-        { Id: uint64
-          FirmId: uint64
-          State: EventState
-          Start: DateTime
-          End: DateTime
-          EntityId: uint64
-          EntityType: EventEntityType }
+        { Id         : int64
+          FirmId     : int64
+          State      : EventState
+          Start      : DateTime
+          End        : DateTime
+          EntityId   : int64
+          EntityType : EventEntityType }
