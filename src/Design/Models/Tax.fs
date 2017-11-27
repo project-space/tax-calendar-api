019fcb74ff024ace259af2e3c78d3f01cfaefcc4
alex troshkin <alextroshkin@outlook.com>
2017-11-28 00:24:45 +0300

    Fill tax types from tax dode

--- src/Design/Models/Tax.fs
+++ src/Design/Models/Tax.fs
@@ -2,11 +2,32 @@ namespace Design.Models
 
 [<AutoOpen>]
 module Tax =
-    
+    open System
+
     (* Уникальные идентификаторы налогов, используются в базе данных - значения не менять *)
+    [<FlagsAttribute>]
     type TaxType =
-        | USN = 0
-        | ENVD = 1
+        (* Федеральные налоги *)
+        | VAT               = 1 (* Налог на добавленную стоимость *)
+        | Excises           = 2 (* Акцизы *)
+        | PersonalIncome    = 3 (* Подоходный налог для физ. лиц *)
+        | CorporateIncome   = 4 (* Налог на прибыль для организаций *)
+        | Water             = 5 (* Водны налог *)
+        | GovernmentDuty    = 6 (* Гос. пошлина *)
+        | MineralExtraction = 7 (* Налог на добычу полезных ископаемых *)
+
+        (* Региональные налоги и сборы *)
+        | Transport         = 8 (* Транспортный налог *)
+        | GamblingBusiness  = 9 (* Налог на игорный бизнес *)
+        | CorporateProperty = 10 (* Налог на имущество для организаций *)
+        
+        (* Местные налоги и сборы *)
+        | Land              = 11 (* Земельный налог *)
+        | PersonalProperty  = 12 (* Налог на имущество для физ. лиц *)
+        | TradingFee        = 13 (* Торговый сбор *)
+
+        (* Страховые взносы в Российской Федерации *)
+        | InsurancePremiums = 14 (* Страховые взносы *)
 
     type TaxDescription =
         { Tax: TaxType
@@ -14,6 +35,7 @@ module Tax =
           Fines: string }
 
     (* Возможные типы налогового периода *)
+    [<FlagsAttribute>]
     type TaxPeriodType =
         | Annual        = 0
         | Quarterly     = 1
