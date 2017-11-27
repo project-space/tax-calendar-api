9d02085dae2196bd15c21556f55b0a489351b818
alex troshkin <alextroshkin@outlook.com>
2017-11-28 01:00:24 +0300

    Fix: invalid connection string

--- src/DataAccess/Core/Connection.fs
+++ src/DataAccess/Core/Connection.fs
@@ -5,10 +5,7 @@ open System.Data.SqlClient
 
 module Connection =
 
-    let private _connectionString = @"
-        Data Source=(localdb)\\mssqllocaldb;
-        Initial Catalog=TaxCalendar;
-        Integrated Security=SSPI;"
+    let private _connectionString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=TaxCalendar;Integrated Security=SSPI;"
 
     let getConnection () : IDbConnection =
         new SqlConnection(_connectionString) :> IDbConnection
