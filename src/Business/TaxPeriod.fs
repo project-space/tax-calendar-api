11c2b3f1ceb8f3e6f51c5bc004882ae25bb95995
alex troshkin <alextroshkin@outlook.com>
2017-12-05 01:25:54 +0300

    Fix project build

--- src/Business/TaxPeriod.fs
+++ src/Business/TaxPeriod.fs
@@ -1,34 +1,34 @@
 namespace Business
 
 module TaxPeriod =
-    open Design.Models.Tax
+    open Design.Models
     open System
 
     //-------------------------------------------------------
     // VAT
-    let private ``create VAT periods`` (tax: Tax) =
-        list<Tax>.Empty
+    let private ``create VAT periods`` (tax: Tax.T) =
+        list<Tax.T>.Empty
 
     //-------------------------------------------------------
     // Composition
     //_______________________________________________________
-    let public Create (taxes: Tax list) =
-        let toTaxPeriods (tax: Tax) =
+    let public Create (taxes: Tax.T list) =
+        let toTaxPeriods (tax: Tax.T) =
             match tax.Id with
-                | TaxType.VAT -> ``create VAT periods`` tax
-                | TaxType.Excises -> failwith "Налог на акцизы не поддерживается на данный момент"
-                | TaxType.PersonalIncome -> failwith "Налог на доходы физ. лиц не поддерживается на данный момент"
-                | TaxType.CorporateIncome -> failwith "Нало на доходы организации не поддерживается на данный момент"
-                | TaxType.Water -> failwith "Водный налог не поддерживается на данный момент"
-                | TaxType.GovernmentDuty -> failwith "Госпошлина не поддерживается на данный момент"
-                | TaxType.MineralExtraction -> failwith "Налог на добычу полезных ископаемых не поддерживается на данный момент"
-                | TaxType.Transport -> failwith "Транспортный налог не поддерживается на данный момент"
-                | TaxType.GamblingBusiness -> failwith "Налог на игорный бизнес не поддерживается на данный момент"
-                | TaxType.CorporateProperty -> failwith "Налог на имущество организации не поддерживается на данный момент"
-                | TaxType.Land -> failwith "Земельны налог не поддерживается на данный момент"
-                | TaxType.PersonalProperty -> failwith "Налог на имущество физических лиц не поддерживается на данный момент"
-                | TaxType.TradingFee -> failwith "Торговый сбор не поддерживается на данный момент"
-                | TaxType.InsurancePremiums -> failwith "Страховые взносы не поддерживаются на данный момент"
+                | Tax.Id.VAT -> ``create VAT periods`` tax
+                | Tax.Id.Excises -> failwith "Налог на акцизы не поддерживается на данный момент"
+                | Tax.Id.PersonalIncome -> failwith "Налог на доходы физ. лиц не поддерживается на данный момент"
+                | Tax.Id.CorporateIncome -> failwith "Нало на доходы организации не поддерживается на данный момент"
+                | Tax.Id.Water -> failwith "Водный налог не поддерживается на данный момент"
+                | Tax.Id.GovernmentDuty -> failwith "Госпошлина не поддерживается на данный момент"
+                | Tax.Id.MineralExtraction -> failwith "Налог на добычу полезных ископаемых не поддерживается на данный момент"
+                | Tax.Id.Transport -> failwith "Транспортный налог не поддерживается на данный момент"
+                | Tax.Id.GamblingBusiness -> failwith "Налог на игорный бизнес не поддерживается на данный момент"
+                | Tax.Id.CorporateProperty -> failwith "Налог на имущество организации не поддерживается на данный момент"
+                | Tax.Id.Land -> failwith "Земельны налог не поддерживается на данный момент"
+                | Tax.Id.PersonalProperty -> failwith "Налог на имущество физических лиц не поддерживается на данный момент"
+                | Tax.Id.TradingFee -> failwith "Торговый сбор не поддерживается на данный момент"
+                | Tax.Id.InsurancePremiums -> failwith "Страховые взносы не поддерживаются на данный момент"
                 | unsupportedTax -> failwith (sprintf "Неподдерживаемый налог [%s]" (unsupportedTax.ToString()))
 
         List.collect toTaxPeriods taxes
