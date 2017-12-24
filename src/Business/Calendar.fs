a5b36e81a27c6c14730101772b0c79f39e60fed5
alex troshkin <alextroshkin@outlook.com>
2017-12-24 22:58:47 +0300

    Generate events: rewrite names in Business -> Calendar

--- src/Business/Calendar.fs
+++ src/Business/Calendar.fs
@@ -6,7 +6,7 @@ open Mappers.Tax.Period
 
 module Calendar =    
 
-    let private ``rebuild events`` (setting : Setting.T) = async {
+    let private rebuildEvents (setting : Setting.T) = async {
         let getRestrictions (period: Tax.Period) = (period, Restrictions.get period.TaxId |> Option.defaultValue [])
         let restrictionsFilter (period: Tax.Period, restrictions) = Restrictions.filter setting.Values restrictions period
 
@@ -18,41 +18,41 @@ module Calendar =
             |> Seq.map (fun (period, _) -> period, ``to event`` setting.FirmId period)
     }
 
-    let private ``get existing events`` (firmId : int64) = async {
-        let! ``existing events`` = DataAccess.Queries.Events.GetAllByFirmId firmId
+    let private getExistingEvents (firmId : int64) = async {
+        let! existingEvents = DataAccess.Queries.Events.GetAllByFirmId firmId
         let periodIds = 
-            ``existing events``
+            existingEvents
             |> Seq.filter (fun event -> event.EntityType = EntityType.TaxPeriod)
             |> Seq.map (fun event -> event.EntityId)
 
         let! periods = DataAccess.Queries.Taxes.Periods.GetAllByIds periodIds
 
-        return ``existing events``
+        return existingEvents
         |> Seq.filter (fun event -> event.EntityType = EntityType.TaxPeriod)
         |> Seq.map (fun event -> periods |> Seq.find (fun period -> event.EntityId = period.Id), event)
     }
 
-    let private ``merge events``
-        (``built events``    : (Tax.Period * Calendar.Event.T) seq)
-        (``existing events`` : (Tax.Period * Calendar.Event.T) seq) =
+    let private mergeEvents
+        (builtEvents    : (Tax.Period * Calendar.Event.T) seq)
+        (existingEvents : (Tax.Period * Calendar.Event.T) seq) =
 
-        let ``events to be removed`` =
-            ``existing events``  |> Seq.filter (fun (eEventPeriod, _) ->
-                ``built events`` |> Seq.exists (fun (bEventPeriod, _) -> eEventPeriod = bEventPeriod) |> not)
+        let toRemove =
+            existingEvents  |> Seq.filter (fun (eEventPeriod, _) ->
+                builtEvents |> Seq.exists (fun (bEventPeriod, _) -> eEventPeriod = bEventPeriod) |> not)
                                  |> Seq.filter (fun (_, eEvent) -> eEvent.State <> State.Completed)
                                  |> Seq.map    (fun (_, eEvent) -> eEvent.Id)
 
-        let ``events to be added`` =
-            ``built events``        |> Seq.filter (fun (bEventPeriod, _) ->
-                ``existing events`` |> Seq.exists (fun (eEventPeriod, _) -> eEventPeriod = bEventPeriod) |> not)
+        let toAdd =
+            builtEvents        |> Seq.filter (fun (bEventPeriod, _) ->
+                existingEvents |> Seq.exists (fun (eEventPeriod, _) -> eEventPeriod = bEventPeriod) |> not)
                                     |> Seq.map    (fun (_, bEvent) -> bEvent)
 
-        (``events to be removed``, ``events to be added``)
+        (toRemove, toAdd)
 
     let rebuild (setting: Setting.T) = async {
-        let! ``built events``    = ``rebuild events`` setting
-        let! ``existing events`` = ``get existing events`` setting.FirmId
-        let  (toRemove, toAdd)   = ``merge events`` ``built events`` ``existing events``
+        let! builtEvents    = rebuildEvents setting
+        let! existingEvents = getExistingEvents setting.FirmId
+        let  toRemove, toAdd   = mergeEvents builtEvents existingEvents
 
-        return (toRemove, toAdd)
+        return toRemove, toAdd
     }
